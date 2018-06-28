e93c6bd8bd572c3fa70fdafd2b9651d6731ebf91
lvmengyao <2283348352@qq.com>
2018-06-28 09:52:33 +0800

    上传新代码

---
+++ src/main/java/com/talentservice/talentservice/TalentserviceApplication.java
@@ -0,0 +1,12 @@
+package com.talentservice.talentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentserviceApplication.class, args);
+	}
+}
